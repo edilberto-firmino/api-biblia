a66e73e34a1db057c8646e850330c838bdc76d3b
edilberti-firmino <edilberto_ti@yahoo.com.br>
2023-04-30 20:45:58 -0300

    alterações sentry

--- routes/api.php
+++ routes/api.php
@@ -37,3 +37,8 @@ Route::get('/versiculo/{id}', [VersiculoController::class,  'show']);
 Route::put('/versiculo/{id}', [VersiculoController::class,  'update']);
 Route::post('/versiculo', [VersiculoController::class,  'store']);
 Route::delete('/versiculo/{id}', [VersiculoController::class,  'destroy']);
+
+
+Route::get('/debug-sentry', function () {
+    throw new Exception('My first Sentry error!');
+});
