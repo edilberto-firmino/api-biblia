27fa9a1a41744aab0945a03732c5d3e43a0aa1f8
edilberti-firmino <edilberto_ti@yahoo.com.br>
2023-05-10 22:23:09 -0300

    nova altreração

--- routes/api.php
+++ routes/api.php
@@ -4,28 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\LivroController;
 use App\Http\Controllers\TestamentoController;
 use App\Http\Controllers\VersiculoController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request)
-// {
-//     return $request->user();
-// });
-// Route::get('/debug-sentry', function () {
-//     throw new Exception('My first Sentry error!');
-// });
 
 
 Route::post('/login',[AuthController::class, 'login']);
