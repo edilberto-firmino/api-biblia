fb64cdc5c6be1e01271158c106b45f95204e7044
edilberti-firmino <edilberto_ti@yahoo.com.br>
2023-05-26 22:14:09 -0300

    tentativa

--- gitpull.php
+++ gitpull.php
@@ -1,13 +1,3 @@
 <?php
-exec('git pull', $output, $return_var);
-
-// Verifica o status de retorno
-if ($return_var === 0) {
-    echo "Comando git pull executado com sucesso.";
-} else {
-    echo "Ocorreu um erro ao executar o comando git pull.";
-}
-
-// Exibe a saída do comando
-echo implode("\n", $output);
+exec('git pull');
 ?>
