85759f43fcc44ad79ce2170d37d444b9bc33b223
edilberti-firmino <edilberto_ti@yahoo.com.br>
2023-05-27 22:06:15 -0300

    alteraÃção de erros

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,12 +9,13 @@ use App\Models\User;
 
 class AuthController extends Controller
 {
-    public function register(Request $request){
-        $fields = $request->validate([
+    public function register(Request $request): \Illuminate\Foundation\Application|\Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory
+    {
+        $fields = $request->validate(array(
             'name'=>'required|string',
             'email'=>'required|string|unique:users,email',
             'password'=>'required|string|confirmed'
-        ]);
+        ));
         $user = User::create([
             'name'=>$fields['name'],
             'email'=>$fields['email'],
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,24 +1,27 @@
 <?php
 
-namespace App\Providers;
+namespace App\Providers {
 
-use Illuminate\Support\ServiceProvider;
+    use Illuminate\Support\ServiceProvider;
+    use Illuminate\Support\Facades\Schema;
 
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Register any application services.
-     */
-    public function register(): void
+
+    class AppServiceProvider extends ServiceProvider
     {
-        //
-    }
+        /**
+         * Register any application services.
+         */
+        public function register(): void
+        {
+            //
+        }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
+        /**
+         * Bootstrap any application services.
+         */
+        public function boot(): void
+        {
+            Schema::defaultStringLength(191);
+        }
     }
 }
